4e548d5655b90f5c0f163318311b42e01da1df31
Ramiro Alvarez <alvarezramiro1997@gmail.com>
2022-05-02 23:01:42 -0300

    added a function called game that plays the function playRound 5 times prompting the user for an answer every time. while counting each win for the computer or the user and then calling the overall winner

--- js/script.js
+++ js/script.js
@@ -17,30 +17,37 @@ function computerPlay() {
     }
 }
 console.log(computerPlay());
-//  now i need to compare the values given by the user and the machine
+//  now i need to compare the values given by the user and the machine and show a message saying who is the winner and what did the machine choose
+let didUserWin;
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == 'rock' && computerSelection == 'rock') {
         return 'its a tie, rock equals rock.';
     }
     else if (playerSelection == 'rock' && computerSelection == 'paper') {
+        didUserWin = 'lose';
         return 'you lose! paper beats rock.';
     }
     else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        didUserWin = 'won';
         return 'you won! rock beats scissors.';
     }
     else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        didUserWin = 'won';
         return 'you won! paper beats rock';
     }
     else if (playerSelection == 'paper' && computerSelection == 'paper') {
         return 'its a tie, paper equals paper.';
     }
     else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        didUserWin = 'lose';
         return 'you lose! scissors beats paper.';
     }
     else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        didUserWin = 'lose';
         return 'you lose! rock beats scissors.';
     }
     else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        didUserWin = 'won';
         return 'you won! scissors beats paper.';
     }
     else if (playerSelection == 'scissors' && computerSelection == 'scissors') {
@@ -50,4 +57,31 @@ function playRound(playerSelection, computerSelection) {
 
 
 }
-// show a message saying who is the winner and what did the machine choose  
+// create a loop to play a 5 round game, in each round i should print out its winner and at last print out the overall winner
+// each round the player should be prompted
+let answerUser;
+let userPoints = 0;
+let computerPoints = 0;
+function game() {
+    for (let i = 0; i < 5; i++) {
+        answerUser.prompt('rock, paper, scissors...');
+        computerPlay();
+        console.log(playRound(answerUser, answerComp));
+        if (didUserWin == 'won') {
+            userPoints += 1;
+        }
+        else if (didUserWin == 'lose') {
+            computerPoints += 1;
+        }
+    }
+    if (userPoints > computerPoints) {
+        return 'you win!';
+    }
+    else if (userPoints < computerPoints) {
+        return 'you lose!';
+    }
+    else {
+        return 'its a tie';
+    }
+}
+console.log(game());  
